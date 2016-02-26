31628cabb384ca17c8fe629c246215bf280bfd8f
Patrick Fung <patrick@makestuffdostuff.com>
2016-02-26 13:10:10 -0500

    comment about elasticsearch cookbook

--- metadata.rb
+++ metadata.rb
@@ -11,5 +11,8 @@ depends 'firewall', '~> 2.4.0'
 depends 'yum-epel', '~> 0.6.5'
 depends 'nginx', '~> 2.7.6'
 depends 'java', '~> 1.39.0'
+# Don't use a more recen't version the elasticsearch cookbook. It's not 
+# confirmed to work with the version of Elasticsearch we are targeting (1.7.x)
+# Refer to: https://github.com/elastic/cookbook-elasticsearch/tree/0.3.x
 depends 'elasticsearch', '~> 0.3.13'
 depends 'nodejs', '~> 2.4.4'
