8f1c39c0aab6d8e0d94eaa901ad7ce5f2ada6388
Patrick Fung <patrick@makestuffdostuff.com>
2016-02-29 15:21:50 -0500

    add attribute for npm modules

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,12 +5,13 @@ default['lits_vm']['allow_web_traffic'] = false
 
 default['lits_vm']['install_mysql'] = false
 default['lits_vm']['install_nginx'] = false
+
 default['lits_vm']['install_nodejs'] = false
+default['lits_vm']['npm_modules'] = nil
 
 default['lits_vm']['install_elasticsearch'] = false
 # TODO: include clase that forces java install if elasticsearch is enabled
 default['lits_vm']['install_java'] = false
-
 
 # PHP
 default['lits_vm']['install_php'] = false
