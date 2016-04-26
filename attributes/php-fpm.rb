863edebd5f8120c1f41b66852243d524be5b2753
Patrick Fung <patrick@makestuffdostuff.com>
2016-04-26 13:44:03 -0400

    use php cookbook to install stuff. sessions don't break anymore with this configuration

--- attributes/php-fpm.rb
+++
@@ -1,6 +0,0 @@
-default['lits_vm']['fpm_pool_config']['user'] = 'nginx'
-default['lits_vm']['fpm_pool_config']['group'] = 'nginx'
-
-default['lits_vm']['fpm_pool_config']['pm']['mode'] = 'ondemand'
-default['lits_vm']['fpm_pool_config']['pm']['max_children'] = 30
-default['lits_vm']['fpm_pool_config']['pm']['process_idle_timeout'] = '10s'
--- recipes/install_php.rb
+++ recipes/install_php.rb
@@ -7,27 +7,26 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Install php manually because the php cookbook doesn't work nicely
-# with php-fpm
-# TODO: un-hardcode these packages
-%w(php php-fpm php-pdo php-mysql php-xml php-mbstring php-apc php-gearman php-ldap).each do |pkg|
+include_recipe 'php'
+
+# install php extensions
+%w(php-pdo php-mysql php-xml php-mbstring php-apc php-gearman php-ldap).each do |pkg|
   package pkg
 end
 
-# Declare php-fpm service
-service 'php-fpm' do
-  service_name 'php-fpm'
-  supports start: true, stop: true, restart: true, reload: true
-  action :start
-end
-
-# Configure php-fpm pools
+# configure pools
 unless node['php']['fpm_pools'].nil?
   node['php']['fpm_pools'].each do |pool|
-    template "#{node['php']['fpm_pooldir']}/#{pool.pool_name}.conf" do
-      source 'php-fpm.pool.erb'
-      variables pool
-      notifies :restart, 'service[php-fpm]'
+    php_fpm_pool pool['pool_name'] do
+      listen pool['listen']
+      process_manager pool['process_manager']
+      max_children pool['max_children']
+      additional_config pool['additional_config']
     end
   end
+end
+
+# make sure the session dir is read/writeable by www user
+directory '/var/lib/php/session' do
+  group node['php']['fpm_group']
 end
