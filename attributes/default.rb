ae7222ca5ee66b5913c4102fdeb583e04c5719db
Patrick Fung <patrick@makestuffdostuff.com>
2016-05-10 13:07:20 -0400

    open up ssh port in firewall

--- attributes/default.rb
+++ attributes/default.rb
@@ -15,8 +15,9 @@ default['lits_vm']['php_extension_packages'] = []
 default['php']['fpm_user']  = node['nginx']['user']
 default['php']['fpm_group'] = node['nginx']['user']
 
-# Disable default nginx site
-default['nginx']['default_site_enabled'] = false
-
 # sudos
 default['authorization']['sudo']['groups'] = ['sysadmin']
+
+## Do not edit below!
+default['firewall']['allow_ssh'] = true
+default['nginx']['default_site_enabled'] = false
