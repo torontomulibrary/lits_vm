503a7a18ec6345b95ce9e4d799a181c5a835ab2a
Patrick Fung <patrick@makestuffdostuff.com>
2016-05-11 13:02:25 -0400

    use data bag to configure mysql service and databases

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -6,6 +6,45 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# Install mysql2_chef_gem to set up databases
+mysql2_chef_gem 'default'
+
+search("#{node.name}_mysql", '*:*') do |mysql_service|
+  # Create and start MySQL instance
+  mysql_service mysql_service['id'] do
+    bind_address mysql_service['bind_address']
+    version mysql_service['version']
+    initial_root_password mysql_service['initial_root_password']
+    action [:create, :start]
+  end
+
+  # Define connection info hash
+  mysql_connection_info = {
+    host:      mysql_service['bind_address'],
+    username: 'root',
+    socket:   "/var/run/mysql-#{mysql_service['id']}/mysqld.sock",
+    password: mysql_service['initial_root_password']
+  }
+
+  # For each database listed in the data bag
+  mysql_service['databases'].each do |database|
+    # create the database
+    mysql_database database['database_name'] do
+      connection mysql_connection_info
+      encoding  'utf8'
+      collation 'utf8_unicode_ci'
+    end
+
+    # create database user and grant all priveleges
+    mysql_database_user database['user'] do
+      connection      mysql_connection_info
+      database_name   database['name']
+      password        database['user_password']
+      action :grant
+    end
+  end
+end
+
 # Create and start MySQL instance
 mysql_service node['mysql']['service_name'] do
   bind_address node['mysql']['bind_address']
@@ -13,9 +52,6 @@ mysql_service node['mysql']['service_name'] do
   initial_root_password node['mysql']['initial_root_password']
   action [:create, :start]
 end
-
-# Install mysql2_chef_gem to set up databases
-mysql2_chef_gem 'default'
 
 unless node['mysql']['databases'].nil?
   node['mysql']['databases'].each do |database, d|
