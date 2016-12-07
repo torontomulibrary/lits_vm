4892bc052945a7ffba7170b5247a2476d8c9fe2c
Patrick Fung <patrick@makestuffdostuff.com>
2016-12-07 15:20:39 -0500

    update mysql recipe to use a better data bag structure

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -11,43 +11,53 @@
 mysql2_chef_gem 'default'
 
 begin
-  search("#{node.name}_mysql", '*:*') do |mysql_service|
-    # Create and start MySQL instance
-    mysql_service mysql_service['id'] do
-      bind_address mysql_service['bind_address']
-      version mysql_service['version']
-      initial_root_password mysql_service['initial_root_password']
+  search("#{node.name}_bags", 'bag_type:mysql_service') do |service|
+    # Configure MySQL service process
+    mysql_service service['name'] do
+      bind_address service['bind_address']
+      version service['version']
+      initial_root_password service['initial_root_password']
       action [:create, :start]
     end
 
-    # Define connection info hash
+    # Define connection info
     mysql_connection_info = {
-      host:      mysql_service['bind_address'],
+      host:      service['bind_address'],
       username: 'root',
-      socket:   "/var/run/mysql-#{mysql_service['id']}/mysqld.sock",
-      password: mysql_service['initial_root_password']
+      socket:   "/var/run/mysql-#{service['name']}/mysqld.sock",
+      password: service['initial_root_password']
     }
 
-    # For each database listed in the data bag
-    mysql_service['databases'].each do |database|
-      # create the database
-      mysql_database database['database_name'] do
+    # Create databases
+    service['databases'].each do |database|
+      mysql_database database['name'] do
         connection mysql_connection_info
         encoding  'utf8'
         collation 'utf8_unicode_ci'
       end
+    end
 
-      # create database user and grant all priveleges
-      mysql_database_user database['user'] do
-        connection      mysql_connection_info
-        database_name   database['name']
-        password        database['user_password']
-        action :grant
+    # Create users
+    service['users'].each do |user|
+      mysql_database_user user['username'] do
+        connection mysql_connection_info
+        password user['password']
+        action :create
+      end
+
+      # Grant priveleges on databases to user
+      user['databases'].each do |db_name|
+        mysql_database_user user['username'] do
+          connection mysql_connection_info
+          database_name db_name
+          privileges [:all]
+          action :grant
+        end
       end
     end
   end
 rescue Net::HTTPServerException, Chef::Exceptions::InvalidDataBagPath
-  log "#{node.name}_mysql data bag not found" do
+  log "#{node.name}_bags not found!" do
     level :info
   end
 end
