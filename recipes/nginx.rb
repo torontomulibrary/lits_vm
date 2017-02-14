3c5e0cf1a5d0fa70bbfa468f4e37a16b76d09671
Patrick Fung <patrick@makestuffdostuff.com>
2017-02-14 17:36:11 -0500

    add basic ssl certificate request process

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -7,23 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Generate a self-signed certificate because nginx breaks
-# when SSL is asked for with missing certificates
-include_recipe 'acme'
-begin
-  search("#{node.name}_bags", 'bag_type:ssl_config') do |ssl|
-    acme_selfsigned ssl.hostname do
-      crt "/etc/ssl/#{ssl}.crt"
-      key "/etc/ssl/#{ssl}.key"
-    end
-  end
-rescue Net::HTTPServerException, Chef::Exceptions::InvalidDataBagPath
-  log "No SSL configuration databags for #{node.name} found" do
-    level :info
-  end
-end
-
 # Configure nginx sites from data bags
+include_recipe 'acme' # for SSL :)
 include_recipe 'chef_nginx'
 directory node['nginx']['default_root'] do
   recursive true
@@ -31,34 +16,59 @@ end
 
 begin
   search("#{node.name}_bags", 'bag_type:nginx_site') do |ns|
+    # create log files
+    file "/var/log/nginx/#{ns['name']}.access.log" do
+      owner node['nginx']['user']
+      group node['nginx']['group']
+      mode '644'
+      action :create_if_missing
+    end
+    file "/var/log/nginx/#{ns['name']}.error.log" do
+      owner node['nginx']['user']
+      group node['nginx']['group']
+      mode '644'
+      action :create_if_missing
+    end
+
+    # Self-signed certificate
+    acme_selfsigned ns['hostname'] do
+      crt "/etc/ssl/#{ns['hostname']}.crt"
+      key "/etc/ssl/#{ns['hostname']}.key"
+      not_if { ns['hostname'].nil? }
+    end if ns['ssl']
+
+    # Webdir + index for LetsEncrypt to verify
+    directory ns['wwwroot'] do
+      owner node['nginx']['user']
+      group node['nginx']['group']
+      recursive true
+    end if ns['ssl']
+    cookbook_file "#{ns['wwwroot']}/index.html" do
+      source 'index.html'
+    end if ns['ssl']
+
     nginx_site ns['name'] do
       template 'nginx-site.erb'
       variables(
         blocks: ns['blocks']
       )
       action :enable
+      notifies :reload, 'service[nginx]', :immediately
     end
+
+    # Request the real certificate!
+    acme_certificate ns['hostname'] do
+      fullchain "/etc/ssl/#{ns['hostname']}.crt"
+      chain     "/etc/ssl/#{ns['hostname']}-chain.crt"
+      key       "/etc/ssl/#{ns['hostname']}.key"
+      wwwroot   ns['wwwroot']
+      owner node['nginx']['user']
+      group node['nginx']['group']
+      notifies :reload, 'service[nginx]'
+    end if ns['ssl']
   end
 rescue Net::HTTPServerException, Chef::Exceptions::InvalidDataBagPath
   log "#{node.name}_nginx data bag not found" do
-    level :info
-  end
-end
-
-# Request the real certificate
-include_recipe 'acme'
-begin
-  search("#{node.name}_bags", 'bag_type:ssl_config') do |ssl|
-    acme_certificate ssl.hostname do
-      fullchain "/etc/ssl/#{ssl.hostname}.crt"
-      chain     "/etc/ssl/#{ssl.hostname}-chain.crt"
-      key       "/etc/ssl/#{ssl.hostname}.key"
-      wwwroot   node['nginx']['default_root']
-      notifies  :reload, 'service[nginx]'
-    end
-  end
-rescue Net::HTTPServerException, Chef::Exceptions::InvalidDataBagPath
-  log "No SSL configuration databags for #{node.name} found" do
     level :info
   end
 end
