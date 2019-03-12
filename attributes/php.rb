46e0529791e3c30df500d5d51415a3fd6795f675
Patrick Fung <patrick@makestuffdostuff.com>
2019-03-12 17:05:39 -0400

    move php attributes into their own file

---
+++ attributes/php.rb
@@ -0,0 +1,4 @@
+default['lits_vm']['php']['fpm_session_dir'] = '/var/lib/php/session'
+default['lits_vm']['php']['pecls'] = []
+default['lits_vm']['php']['install_gearman_extension'] = false
+default['lits_vm']['php']['install_apcu_extension'] = false
